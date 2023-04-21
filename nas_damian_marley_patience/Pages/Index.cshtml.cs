db29cd2a4dfe556df676adddbe6c6b0fef942f90
3nDr0n <magyarali@gmail.com>
2023-04-21 06:55:19 +0100

    Add project files.

---
+++ nas_damian_marley_patience/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nas_damian_marley_patience.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
